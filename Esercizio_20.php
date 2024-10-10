ab43f11aeb9d59e23f14f5d0dad01b7177ea0f05
Davide Susini <susini.davide@itismeucci.com>
2024-10-10 10:42:09 +0200

    esercizio finito

--- Esercizio_20.php
+++ Esercizio_20.php
@@ -6,6 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <table style="border-collapse: collapse">
+        <tr>
+            <th style="border: 1px solid black">Indirizzo</th>
+            <th style="border: 1px solid black">Metri quadri</th>
+            <th style="border: 1px solid black">Prezzo metro quadro</th>
+            <th style="border: 1px solid black">Prezzo tot</th>
+        </tr>
+        <tr>
+    <?php
+        $casa = array("indirizzo" => "Via Ciarda 63", "metri_quadri" => 104.63, "prezzoMQ" => 2663);
+        $prezzo_tot = $casa["metri_quadri"] * $casa["prezzoMQ"];
+        $casa["prezzoTOT"] = $prezzo_tot;
+        
+        foreach ($casa as $value) {
+            echo "<td style='border: 1px solid black'> $value </td>";
+        }
+        
+    ?>
+    </tr>
+    </table>
 </body>
 </html>
